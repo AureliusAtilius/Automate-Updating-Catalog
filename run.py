1eee4edff23985b4e97eceeb84d744a3e0c49eb4
Josh <j.j.h.lindner@gmail.com>
2021-05-25 21:27:49 -0700

    Complete script

--- run.py
+++ run.py
@@ -10,3 +10,9 @@ def processDescription(file):
                 jsonobj["weight"]= int(lines[1].rstrip("\n"))
                 jsonobj["description"]=lines[2].rstrip("\n")
                 jsonobj["image_name"]=lines[3].rstrip("\n")
+        url="http://[linux-instance-IP-Address]/fruits"
+        response= requests.post(url, json=p)
+
+if __name__=="__main__":
+        for file in os.listdir("~/supplier-data/descriptions"):
+                processDescription(file)
