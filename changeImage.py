c69795309eb693e66e67baffa2d3e5806572edb6
Josh <j.j.h.lindner@gmail.com>
2021-05-30 15:29:30 -0700

    Debug changeImage.py
    Use full filepaths.
    Save new image as jpeg and delete original file.

--- changeImage.py
+++ changeImage.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python3
 import os
-import requests
 from PIL import Image
 
 def imageReformat(file):
         # Check if .TIFF file
-        filename, fileExt = os.path.splitext(file)
-        if fileExt == ".TIFF":
-                im = Image.open(file)
+        filename, fileExt = os.path.splitext("/home/{}/supplier-data/images/".format(os.environ.get('USER'))+file)
+        if fileExt == ".tiff":
+                im = Image.open("/home/{}/supplier-data/images/".format(os.environ.get('USER'))+file)
                 resized_im = im.resize((600,400))
                 #cannot convert from TIFF to JPEG, must convert to RGB first
                 resized_im = resized_im.convert("RGB")
-        
-                resized_im.save("~/supplier-data/images/"+filename, format= "JPEG")
-        
+
+                resized_im.save(filename+'.jpeg', format= "JPEG")
+                os.remove("/home/{}/supplier-data/images/".format(os.environ.get('USER'))+filename+fileExt)
 
                 
 if __name__=="__main__":
-        for file in os.listdir("~/supplier-data/images"):
+        for file in os.listdir("/home/{}/supplier-data/images".format(os.environ.get('USER'))):
                 imageReformat(file)
