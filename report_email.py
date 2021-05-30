853deb740c4dcd2187d331cea44ff85360fde59b
Josh <j.j.h.lindner@gmail.com>
2021-05-29 17:17:00 -0700

    Create report_email.py
    Adjust reports.py so it is only responsible for assembling the framework
    of the report and building it.
    
    Report_email.py is responsible for processing the data and formatting it
    in preparation to be passed to reports.generate_report.py

---
+++ report_email.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from reportlab.platypus import paragraph
+import reports
+import run
+
+
+def  processReport():
+        pdf_lines=[]
+        for file in os.listdir("~/supplier-data/descriptions"):
+                data=run.processDescription(file)
+                name = "name: {}".format(data["name"])
+                weight = "weight: {}".format(data["weight"])
+                empty_line= "<br/>"
+                pdf_lines.extend([name, weight,empty_line])
+        pdf_data="<br/>".join(pdf_lines)        
+        return pdf_data
+if __name__=="__main__":
+        title= "Uploaded Items"
+        body = processReport()
+        reports.generate_report("/tmp/processed.pdf",title,body)
--- reports.py
+++ reports.py
@@ -5,15 +5,13 @@ from reportlab.platypus import SimpleDocTemplate
 from reportlab.platypus import Paragraph, Spacer
 from reportlab.lib.styles import getSampleStyleSheet
 
-def reportgenerator():
+def generate_report(attachment, title,body):
         styles = getSampleStyleSheet()
-        report = SimpleDocTemplate("processed.pdf")     #TODO: Add full file path to saved file
+        report = SimpleDocTemplate(attachment)     
+        report_title = Paragraph(title, styles["h1"])
+        report_info = Paragraph(body, styles["BodyText"])
         empty_line = Spacer(1,20)
-        pdf_file=[]
-        for file in os.listdir("~/supplier-data/descriptions"):
-                data=run.processDescription(file)
-                name = Paragraph(data["name"], styles["BodyText"])
-                weight = Paragraph(data["weight"], styles["BodyText"])
-                pdf_file.extend([name, weight,empty_line])
+        pdf_file=[report_title,empty_line, report_info]
+        
         report.build(pdf_file)
                 
