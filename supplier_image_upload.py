36e2cdedf162d1c159179fb2a7ffe21303bccd5f
Josh <j.j.h.lindner@gmail.com>
2021-05-30 16:22:47 -0700

    Complete supplier_image_upload.py

--- supplier_image_upload.py
+++ supplier_image_upload.py
@@ -2,12 +2,12 @@
 import requests
 import os
 def imageUploader(image):
-        url= "http://[linux-instance-IP-Address]/upload"    #TODO: Add IP
+        url= "http://localhost/upload/"    
         with open(image,"rb") as image:
                r= requests.post(url, files={"file":image})
 
 if __name__=="__main__":
-        for file in os.listdir("~/supplier-data/images/"):
-                filename, fileExt = os.path.splitext(file)
-                if fileExt == ".jpg":
-                        imageUploader(file)
+        for file in os.listdir("/home/{}/supplier-data/images/".format(os.environ.get('USER'))):
+                filename, fileExt = os.path.splitext("/home/{}/supplier-data/images/".format(os.environ.get('USER'))+file)
+                if fileExt == ".jpeg":
+                        imageUploader(filename+fileExt)
