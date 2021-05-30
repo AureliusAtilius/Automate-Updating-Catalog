942d48ddc62d6633528c8b99e1e664ac373c7bec
Josh <j.j.h.lindner@gmail.com>
2021-05-30 16:22:20 -0700

    Complete run.py

--- run.py
+++ run.py
@@ -3,21 +3,22 @@ import requests
 import os
 
 def processDescription(file):
-        jsonobj={"name": "","weight":0,"description":"","image_name":""}
+        filename, fileExt = os.path.splitext(file)
+        file=("/home/{}/supplier-data/descriptions/".format(os.environ.get('USER'))+file)
+        jsonobj={}
         with open(file,"r") as file:
                 lines = file.readlines()
                 jsonobj["name"]=lines[0].rstrip("\n")
-                jsonobj["weight"]= int(lines[1].rstrip("\n"))
+                jsonobj["weight"]= int((lines[1].rstrip("\n").replace(" lbs","")))
                 jsonobj["description"]=lines[2].rstrip("\n")
-                jsonobj["image_name"]=lines[3].rstrip("\n")   #TODO: Might not have image_name in file.
+                jsonobj["image_name"]=  filename+".jpeg"
         return jsonobj
 
-def uploadDescription(json):
-        jsonobj=json
-        url="http://[linux-instance-IP-Address]/fruits"   #TODO: Add IP
+def uploadDescription(jsonobj):
+        url="http://[linux-instance-IP-Address]/fruits/"   #TODO: Add IP
         response= requests.post(url, json=jsonobj)  
-
+        print(response.status_code)
 if __name__=="__main__":
-        for file in os.listdir("~/supplier-data/descriptions"):
+        for file in os.listdir("/home/{}/supplier-data/descriptions/".format(os.environ.get('USER'))):
                 processed=processDescription(file)
                 uploadDescription(processed)
